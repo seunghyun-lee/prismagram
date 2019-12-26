5f5a127dd5a3d1346b8207d7898c9b68138090d2
seunghyun <seunghyunleekr@gmail.com>
2019-12-26 17:53:14 +0900

    toggleLike & passport jwt

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -3,15 +3,19 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
     Mutation: {
-        requestSecret: async (_, args) => {
+        requestSecret: async (_, args, { request }) => {
             const { email } = args;
             const loginSecret = generateSecret();
+            
+            // console.log(loginSecret);
 
             try {
+                // throw Error();
                 await sendSecretMail(email, loginSecret);
                 await prisma.updateUser({ data: {loginSecret}, where: { email } });
                 return true;
-            } catch {
+            } catch (error) {
+                console.log(error);
                 return false;
             }
         }
--- src/env.js
+++ src/env.js
@@ -1,2 +1,3 @@
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
