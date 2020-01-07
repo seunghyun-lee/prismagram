dd9a99ae114fb47fb967f1c3ba2121fa190e4175
seunghyun <seunghyunleekr@gmail.com>
2020-01-07 16:52:38 +0900

    Receive Message

--- src/api/Message/newMessage/newMessage.js
+++ src/api/Message/newMessage/newMessage.js
@@ -14,7 +14,7 @@ export default {
                             }
                         }
                     ]
-                });
+                }).node();
             },
             resolve: payload => payload
         }
